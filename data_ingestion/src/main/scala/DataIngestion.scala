52035aedf1c5f65b54d586972b255605b021e40c
Clovis Lechien <clovis.lechien@epita.fr>
2024-11-09 11:16:13 +0100

    feat(main): added the HDFS to the main in data_ingestion. Once the hdfs is running this main needs to be called only once.

--- data_ingestion/src/main/scala/DataIngestion.scala
+++ data_ingestion/src/main/scala/DataIngestion.scala
@@ -10,11 +10,17 @@ object DataIngestion {
       .master("local[*]")  // Use all cores
       .getOrCreate()
 
+    spark.sparkContext.setLogLevel("ERROR")
+
     val dataPath = "ecommerce_data_with_trends.csv"
+    val hdfsPath = "hdfs://namenode:9000/user/hadoop/ecommerce_data"  // HDFS saved data path
 
     val df: DataFrame = loadData(spark, dataPath)
 
     df.show(10)
+
+    writeDataToHDFS(df, hdfsPath)
+
     spark.stop()
   }
 
@@ -24,10 +30,16 @@ object DataIngestion {
    * @param path - Path to the data file
    * @return DataFrame containing the loaded data
    */
-  def loadData(spark: SparkSession, path: String): DataFrame = {
+  private def loadData(spark: SparkSession, path: String): DataFrame = {
     spark.read
       .option("header", "true")
       .option("inferSchema", "true")
       .csv(path)
+  }
+
+  private def writeDataToHDFS(df: DataFrame, hdfsPath: String): Unit = {
+    df.write
+      .mode("overwrite")
+      .parquet(hdfsPath)  // Save as Parquet file, we can change this of we need another type of file.
   }
 }
